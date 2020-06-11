568a6b9b8fd0a063c9fd7649d231a489bec6b3a9
meyges <mariya.sf@gmail.com>
2020-06-10 18:00:17 -0700

    adding boiler plate for server

---
+++ server/server.js
@@ -0,0 +1,10 @@
+
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.use(express.static(path.resolve(__dirname, '../dist')))
+//app.get('/', (req, res) => res.send('hello world'))
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))

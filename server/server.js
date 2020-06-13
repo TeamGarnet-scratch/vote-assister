2bf8c0e128e3d9c9e1e4e8ec79de40adc41db621
Tai N <ndhuutai1@gmail.com>
2020-06-13 12:59:42 -0700

    renamed entry file to server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-
 const path = require('path');
 const express = require('express');
 const app = express();
 
 const port = 3000;
 
-app.use(express.static(path.resolve(__dirname, '../dist')))
+app.use(express.static(path.resolve(__dirname, '../dist')));
 //app.get('/', (req, res) => res.send('hello world'))
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);

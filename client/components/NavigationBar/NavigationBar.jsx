ce78026706535de6d134d959e356014bff6f801c
Tai N <ndhuutai1@gmail.com>
2020-06-13 11:13:52 -0700

    added Layout, NavBar, and Footer components along with their style modules

---
+++ client/components/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Navigation.css';
+
+export default (props) => {
+  return (
+    <div className={styles.NavBar}>
+      <div>Vote!</div>
+    </div>
+  );
+};

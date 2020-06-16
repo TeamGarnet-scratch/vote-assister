2346f51c62f8604da6fece9f6c8edbe9369fcefd
olanrewaju Makinde <lanre.developer@gmail.com>
2020-06-16 03:35:28 +0100

    changed directory structure

--- client/components/App.js
+++ client/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Layout from './Layout/Layout.jsx';
+import AddressComponent from './address-search/address-search';
 
 class App extends Component {
   constructor(props) {
@@ -13,10 +14,37 @@ class App extends Component {
   render() {
     return (
       <Layout>
-        Anything goes in here will be centered both vertically and horizontally
-        since the Layout Component has display of flex.
+        {/* Anything goes in here will be centered both vertically and horizontally
+        since the Layout Component has display of flex. */}
+        <AddressComponent onSubmit={this.onSubmit}>hi</AddressComponent>
       </Layout>
     );
   }
 }
 export default App;
+
+// import React, { Component } from 'react';
+// import Layout from './Layout/Layout.jsx';
+// import NavBar from './NavigationBar/NavigationBar.jsx';
+// import Footer from './Footer/Footer.jsx';
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.onSubmit = this.onSubmit.bind(this);
+//   }
+//   onSubmit(event) {
+//     event.preventDefault(event);
+//     console.log('form submitted');
+//   }
+//   render() {
+//     return (
+//       <Layout>
+//         <NavBar />
+
+//         <Footer />
+//       </Layout>
+//     );
+//   }
+// }
+// export default App;

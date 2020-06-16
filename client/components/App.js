460039a936487eb453d7c379b939a2de69cf8539
olanrewaju Makinde <lanre.developer@gmail.com>
2020-06-16 03:16:38 +0100

    mounted address component to main app

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,15 @@
-import React, { Component } from "react";
-import Layout from "./Layout/Layout.jsx";
+import React, { Component } from 'react';
+import Layout from './Layout/Layout.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onSubmit(event) {
+    event.preventDefault(event);
+    console.log('form submitted');
+  }
   render() {
     return (
       <Layout>

e102407b64b253c4be4e76d387125073ac9347e7
olanrewaju Makinde <lanre.developer@gmail.com>
2020-06-18 01:52:53 +0100

    setup summary component struc

--- client/components/civic-summary/civic-summary.jsx
+++ client/components/civic-summary/civic-summary.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './civic-summary.css';
 
 const CivicSummary = () => {
-  return <div className={styles.main_banner}></div>;
+  return <div className={styles.main__banner}></div>;
 };
 
 export default CivicSummary;

7e42969604db3dec9b3c8385e66905af43db75c4
Tai N <ndhuutai1@gmail.com>
2020-06-18 17:54:10 -0700

    converted to functional component with AirBnB style

---
+++ client/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from './Layout/Layout';
+
+export const App = () => ((
+  <Layout>
+    Anything goes in here will be centered both vertically and horizontally
+    since the Layout Component has display of flex.
+  </Layout>
+));
+export default App;

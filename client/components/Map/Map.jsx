6c63e097b926483d98d7add4e557e710fd507a3f
Tai N <ndhuutai1@gmail.com>
2020-06-20 13:50:34 -0700

    changed Map implementation to work with more data

--- client/components/Map/Map.jsx
+++ client/components/Map/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, memo } from 'react';
 import {
   LoadScript,
   GoogleMap,
@@ -9,13 +9,40 @@ import {
 import styled from 'styled-components';
 import mapStyle from './MapStyle';
 
+class LoadScriptOnlyIfNeeded extends LoadScript {
+  componentDidMount() {
+    const cleaningUp = true;
+    const isBrowser = typeof document !== 'undefined'; // require('@react-google-maps/api/src/utils/isbrowser')
+    const isAlreadyLoaded =
+      window.google &&
+      window.google.maps &&
+      document.querySelector('body.first-hit-completed'); // AJAX page loading system is adding this class the first time the app is loaded
+    if (!isAlreadyLoaded && isBrowser) {
+      // @ts-ignore
+      if (window.google && !cleaningUp) {
+        console.error('google api is already presented');
+        return;
+      }
+
+      this.isCleaningUp().then(this.injectScript);
+    }
+
+    if (isAlreadyLoaded) {
+      this.setState({ loaded: true });
+    }
+  }
+}
+
 const options = {
   styles: mapStyle,
 };
 
 const mapContainerStyle = {
-  height: '100vh',
-  width: '100%',
+  height: `80vh`,
+  width: `45%`,
+  position: 'absolute',
+  top: '50%',
+  transform: 'translate(-15%,-50%)',
 };
 const InfoWrap = styled.div`
   display: ${(props) => props.display || 'flex'};
@@ -41,40 +68,47 @@ const InfoWrapCellTittleOneColumns = styled.h3`
   font-size: 1em;
 `;
 
-const InfoBoxContent = (props) => (
-  <div className="info-wrap">
-    <InfoWrapTittle>Location Name</InfoWrapTittle>
-    <InfoWrap>
-      <InfoWrapCellTittlePoll>poll hours</InfoWrapCellTittlePoll>
-      <div>
-        <strong>8AM - 6PM</strong>
-      </div>
-    </InfoWrap>
-    <InfoWrap>
-      <InfoWrapCellTittleTwoColumns>Address:</InfoWrapCellTittleTwoColumns>
-      <div>
-        Eisenhower Recreation Center <br />
-        4300 E Dartmouth Ave
-        <br />
-        Denver, CO
-      </div>
-    </InfoWrap>
-    <InfoWrap display="inline-block">
-      <InfoWrapCellTittleTwoColumns>Notes:</InfoWrapCellTittleTwoColumns>
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut sem
-        quis purus tristique fermentum ac in urna. Nam sodales, augue eget
-        mollis tincidunt, erat risus blandit elit, id pharetra libero ligula sit
-        amet sapien.
-      </div>
-    </InfoWrap>
-    {/* <h3>{props.title}</h3>
-    <div>{props.id}</div>
-    <div>{props.desc}</div> */}
-  </div>
-);
+const InfoBoxContent = (props) => {
+  console.log('INFOBOXCONTENT', props);
 
-const Map = () => {
+  return (
+    <div className="info-wrap">
+      <InfoWrapTittle>{props.address.locationName}</InfoWrapTittle>
+      <InfoWrap>
+        <InfoWrapCellTittlePoll>poll hours</InfoWrapCellTittlePoll>
+        <div>
+          <strong>{props.pollingHours}</strong>
+        </div>
+      </InfoWrap>
+      <InfoWrap>
+        <InfoWrapCellTittleTwoColumns>Address:</InfoWrapCellTittleTwoColumns>
+        <div>
+          {props.address.line1}
+          <br />
+          {props.address.city}, {props.address.state} {props.address.zip}
+        </div>
+      </InfoWrap>
+      <InfoWrap display="inline-block">
+        <InfoWrapCellTittleTwoColumns>Notes:</InfoWrapCellTittleTwoColumns>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut sem
+          quis purus tristique fermentum ac in urna. Nam sodales, augue eget
+          mollis tincidunt, erat risus blandit elit, id pharetra libero ligula
+          sit amet sapien.
+        </div>
+      </InfoWrap>
+      {/* <h3>{props.title}</h3>
+      <div>{props.id}</div>
+      <div>{props.desc}</div> */}
+    </div>
+  );
+};
+
+const Map = ({ mapData }) => {
+  console.log('MAP DATA', mapData);
+
+  const { pollingLocations } = mapData;
+
   const initPosition = {
     lat: 36.2111059,
     lng: -113.7191045,
@@ -102,25 +136,25 @@ const Map = () => {
     },
   ];
 
-  const [markers, setMarkerMap] = useState([markersDefault]);
+  const [markers, setMarkerMap] = useState(null);
   const [infoOpen, setInfoOpen] = useState(null);
   const [selectedPlace, setSelectedPlace] = useState(null);
 
   const mapRef = useRef(null);
-  const [zoom, setZoom] = useState(4);
+  const [zoom, setZoom] = useState(8);
   const handleMapLoaded = (map) => {
     mapRef.current = map;
   };
 
-  const markerLoadHandler = (marker, place) => {
+  const markerLoadHandler = (marker, location) => {
     return setMarkerMap((prevState) => {
-      return { ...prevState, [place.id]: marker };
+      return { ...prevState, [location.address.locationName]: marker };
     });
   };
 
-  const markerClickHandler = (event, place) => {
+  const markerClickHandler = (event, location) => {
     // Remember which place was clicked
-    setSelectedPlace(place);
+    setSelectedPlace(location);
     // Required so clicking a 2nd marker works as expected
     if (infoOpen) {
       setInfoOpen(false);
@@ -134,7 +168,7 @@ const Map = () => {
 
   return (
     <div>
-      <LoadScript
+      <LoadScriptOnlyIfNeeded
         googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
         language="en"
         region="us"
@@ -142,16 +176,22 @@ const Map = () => {
         <GoogleMap
           onLoad={handleMapLoaded}
           mapContainerStyle={mapContainerStyle}
-          center={initPosition}
+          center={{
+            lat: parseFloat(mapData.userLocation.lat),
+            lng: parseFloat(mapData.userLocation.long),
+          }}
           options={options}
-          zoom={4}
+          zoom={8}
         >
-          {markers[0].map((place) => (
+          {pollingLocations.map((location) => (
             <Marker
-              key={place.id}
-              position={place.position}
-              onLoad={(marker) => markerLoadHandler(marker, place)}
-              onClick={(marker) => markerClickHandler(marker, place)}
+              key={location.address.locationName}
+              position={{
+                lat: location.address.latitude,
+                lng: location.address.longitude,
+              }}
+              onLoad={(marker) => markerLoadHandler(marker, location)}
+              onClick={(marker) => markerClickHandler(marker, location)}
               icon={{
                 path:
                   'M4 24h-2v-24h2v24zm18-21.387s-1.621 1.43-3.754 1.43c-3.36 0-3.436-2.895-7.337-2.895-2.108 0-4.075.98-4.909 1.694v12.085c1.184-.819 2.979-1.681 4.923-1.681 3.684 0 4.201 2.754 7.484 2.754 2.122 0 3.593-1.359 3.593-1.359v-12.028z',
@@ -165,20 +205,19 @@ const Map = () => {
 
           {infoOpen && selectedPlace && (
             <InfoWindow
-              anchor={markers[selectedPlace.id]}
+              anchor={markers[selectedPlace.address.locationName]}
               onCloseClick={() => setInfoOpen(false)}
             >
               <InfoBoxContent
-                id={selectedPlace.id}
-                title={selectedPlace.title}
-                desc={selectedPlace.desc}
+                id={selectedPlace.address.locationName}
+                {...selectedPlace}
               />
             </InfoWindow>
           )}
         </GoogleMap>
-      </LoadScript>
+      </LoadScriptOnlyIfNeeded>
     </div>
   );
 };
 
-export default Map;
+export default memo(Map);
